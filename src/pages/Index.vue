dd5dec624db94d74a9cf7854b41fdeeba12ba289
backurdi <bachir098@gmail.com>
2020-06-01 17:28:06 +0200

    Added carousel to home page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -3,14 +3,39 @@
     <img class="greet-image" src="../../uploads/hello.svg" />
 
     <h2 class="mb-4">Gridsome starter bootstrap</h2>
+
+    <Carousel>
+      <slide>
+        <p>Hello</p>
+      </slide>
+      <slide>
+        <p>World</p>
+      </slide>
+      <slide>
+        <p>World</p>
+      </slide>
+      <slide>
+        <p>World</p>
+      </slide>
+      <slide>
+        <p>World</p>
+      </slide>
+    </Carousel>
   </Layout>
 </template>
 
 <script>
+import { Carousel, Slide } from "vue-carousel";
+
 export default {
+  components: {
+    Carousel,
+    Slide,
+  },
+
   metaInfo: {
-    title: "Gridsome starter bootstrap"
-  }
+    title: "Gridsome starter bootstrap",
+  },
 };
 </script>
 
